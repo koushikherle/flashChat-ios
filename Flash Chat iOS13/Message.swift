6b629e5a970d58aed3aa7e0862f8113947482d2f
Koushik <koushik.herale32@gmail.com>
2021-07-10 23:07:15 +0530

    First commit

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Koushik Herle on 10/07/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender : String // 1@2.com
+    let body: String
+}
---
+++ Podfile
@@ -0,0 +1,13 @@
+
+target 'Flash Chat iOS13' do
+
+use_frameworks!
+
+pod 'CLTypingLabel', '~> 0.4.0'
+
+pod 'Firebase/Auth'
+
+pod 'Firebase/Firestore'
+
+
+end
